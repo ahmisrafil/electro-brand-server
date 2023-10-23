11ccecab737acb87026d21d7265d950e3137c55f
ahmisrafil <ahmisrafil@yahoo.com>
2023-10-24 02:17:46 +0600

    delete operation added for cart items

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -62,6 +62,16 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
+
+
+    // delete a product
+    app.delete('/cart/:id', async(req,res)=>{
+        const id = req.params.id;
+        const  query= {_id: new ObjectId(id)};
+        const result=await cartCollection.deleteOne(query);
+        res.send(result);
+    })
+
 
 
 
